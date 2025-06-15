e9f0fe309ab21878147adf39f024da2035eced9e
24155-anas <bscs24155@itu.edu.pk>
2025-06-15 21:37:34 +0500

    Project version 2
    minor changes

---
+++ projV2/main.cpp
@@ -0,0 +1,37 @@
+#include "raylib.h"
+#include "MiniGameArcade.h"
+using namespace std;
+
+
+
+
+
+int main1() {
+    ttt_HumanPlayer p1('O');
+    ttt_HumanPlayer p2('X');
+    ttt_GM game(&p1, &p2);
+
+    char choice;
+    do {
+        game.play();
+        cout << "Play again? (y/n): ";
+        cin >> choice;
+    } while (choice == 'y');
+
+    return 0;
+}
+
+
+// Example usage in main.cpp:
+
+int main() {
+    MiniGameArcade arcade;
+    arcade.runArcade();
+    return 0;
+}
+
+
+
+
+
+
